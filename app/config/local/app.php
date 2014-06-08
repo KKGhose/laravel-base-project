74dcbd2c36813f199f16f87654de1b3023db9e9d
shin1x1 <shin1x1@gmail.com>
2014-06-08 14:53:19 +0900

    add Laravel 4 Debuger

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -4,6 +4,7 @@ return [
     'debug' => true,
     'providers' => append_config([
         'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+        'Barryvdh\Debugbar\ServiceProvider',
         'Way\Generators\GeneratorsServiceProvider',
     ]),
 ];
